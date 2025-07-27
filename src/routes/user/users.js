be31f7652ed4ee2596eeb50c888f320ef19f484d
Kilyong Jeong <uzoolove@gmail.com>
2025-07-28 00:27:22 +0900

    .

--- src/routes/user/users.js
+++ src/routes/user/users.js
@@ -217,7 +217,7 @@ router.post('/signup/oauth', [
     }]
 
     #swagger.requestBody = {
-      description: "회원 정보가 저장된 객체입니다.<br>id: Provider가 제공하는 사용자 id(필수, Auth.js를 사용할 경우 account.providerAccountId)<br>type: 회원 구분(필수, 구매회원: user, 판매회원: seller)<br>loginType: 인증 제공자(필수, google, github 등)<br>email: 이메일(선택)<br>name: 이름(선택)<br>image: 프로필 이미지(선택)<br>extra: 추가 데이터(선택). 추가하고 싶은 아무 속성이나 지정",
+      description: "회원 정보가 저장된 객체입니다.<br>type: 회원 구분(필수, 구매회원: user, 판매회원: seller)<br>loginType: 인증 제공자(필수, google, github 등)<br>email: 이메일(선택)<br>name: 이름(선택)<br>image: 프로필 이미지(선택)<br>extra: 추가 데이터(선택). 추가하고 싶은 아무 속성이나 지정<br>extra.providerAccountId: Provider가 제공하는 사용자 id(필수, Auth.js를 사용할 경우 account.providerAccountId)",
       required: true,
       content: {
         "application/json": {
