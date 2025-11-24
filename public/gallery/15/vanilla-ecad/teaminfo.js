f56ea73bdb8926ec5947986d30a8b2363db086b1
kilyong jeong <uzoolove@gmail.com>
2025-11-24 13:03:54 +0900

    .

---
+++ public/gallery/15/vanilla-ecad/teaminfo.js
@@ -0,0 +1,156 @@
+export default {
+  teams: [
+    {
+      id: 1,
+      name: "코딩네스푼",
+      leader: "김하연",
+      leaderRole: "PM",
+      members: [
+        { name: "이하영", role: "PL" },
+        { name: "한정아", role: "문서" },
+        { name: "김현수", role: "발표" }
+      ],
+      projectName: "나이키",
+      projectTopic: "나이키 클론 프로젝트",
+      notionLink: "https://www.notion.so/1-2a773873401a80e0aaf8fef33106f285",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-01-nike",
+      figmaLink: "",
+      deployUrl: "https://cfs-nike1.netlify.app/"
+    },
+    {
+      id: 2,
+      name: "인스턴스",
+      leader: "김채희",
+      leaderRole: "PM",
+      members: [
+        { name: "방명규", role: "PL" },
+        { name: "김여경", role: "문서" },
+        { name: "박지섭", role: "발표" }
+      ],
+      projectName: "브런치",
+      projectTopic: "브런치 클론 프로젝트",
+      notionLink: "https://www.notion.so/2-2a773873401a80e19aa2fb633dff7a87",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-02-brunch-clone",
+      figmaLink: "",
+      deployUrl: "https://instance02-brunch.netlify.app/"
+    },
+    {
+      id: 3,
+      name: "비동기들",
+      leader: "박한섭",
+      leaderRole: "PM",
+      members: [
+        { name: "소혜림", role: "PL" },
+        { name: "박예슬", role: "문서" },
+        { name: "오지아", role: "발표" }
+      ],
+      projectName: "브런치",
+      projectTopic: "브런치 클론 프로젝트",
+      notionLink: "https://www.notion.so/3-2a473873401a80b9a8b2ed360a0a7a92",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-03-thecallbackproject",
+      figmaLink: "",
+      deployUrl: "https://thecallbackproject.netlify.app/"
+    },
+    {
+      id: 4,
+      name: "아이쑤크림",
+      leader: "류혜진",
+      leaderRole: "PM",
+      members: [
+        { name: "장유석", role: "PL" },
+        { name: "백승준", role: "문서" },
+        { name: "채민기", role: "발표" }
+      ],
+      projectName: "브런치",
+      projectTopic: "브런치 클론 프로젝트",
+      notionLink: "https://www.notion.so/4-2a773873401a80609338f851a2b2cb51",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-04-brunch",
+      figmaLink: "",
+      deployUrl: "https://vanilla-04.netlify.app/"
+    },
+    {
+      id: 5,
+      name: "NaNnilla",
+      leader: "서주희",
+      leaderRole: "PM",
+      members: [
+        { name: "이유진B", role: "PL" },
+        { name: "이유진A", role: "문서" },
+        { name: "유현욱", role: "발표" }
+      ],
+      projectName: "나이키",
+      projectTopic: "나이키 클론 프로젝트",
+      notionLink: "https://www.notion.so/5-NaNnilla-2a773873401a800f8061d77f9fe51629",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-05-NanNilla",
+      figmaLink: "",
+      deployUrl: "https://nike-vanilla.netlify.app/"
+    },
+    {
+      id: 6,
+      name: "init",
+      leader: "고은별",
+      leaderRole: "PM",
+      members: [
+        { name: "장수정", role: "PL" },
+        { name: "조시연", role: "문서" },
+        { name: "문혁진", role: "발표" }
+      ],
+      projectName: "브런치",
+      projectTopic: "브런치 클론 프로젝트",
+      notionLink: "https://www.notion.so/6-init-2a773873401a8028ae03e7a1d14b43f8",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-06-init.git",
+      figmaLink: "",
+      deployUrl: "https://vanilla-06-init.netlify.app/"
+    },
+    {
+      id: 7,
+      name: "promise",
+      leader: "김현주",
+      leaderRole: "PM",
+      members: [
+        { name: "이승규", role: "PL" },
+        { name: "김은재", role: "문서" },
+        { name: "윤소라", role: "발표" }
+      ],
+      projectName: "브런치",
+      projectTopic: "브런치 클론 프로젝트",
+      notionLink: "https://www.notion.so/7-promise-2a473873401a80db84e5da1a01ef2e4f",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-07-promise",
+      figmaLink: "",
+      deployUrl: "https://07-promise.netlify.app/"
+    },
+    {
+      id: 8,
+      name: "I like E",
+      leader: "함초롱",
+      leaderRole: "PM",
+      members: [
+        { name: "김지안", role: "PL, 문서" },
+        { name: "이수진", role: "발표" }
+      ],
+      projectName: "브런치",
+      projectTopic: "브런치 클론 프로젝트",
+      notionLink: "https://www.notion.so/8-I-like-E-2a773873401a80ef842df4df49ac7195",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-08-i-like-e",
+      figmaLink: "",
+      deployUrl: "https://i-like-e.netlify.app/"
+    },
+    {
+      id: 9,
+      name: "CodeZen",
+      leader: "김지유",
+      leaderRole: "PM",
+      members: [
+        { name: "유홍상", role: "PL" },
+        { name: "박지은", role: "문서" },
+        { name: "허예진", role: "발표" }
+      ],
+      projectName: "나이키",
+      projectTopic: "나이키 클론 프로젝트",
+      notionLink: "https://www.notion.so/9-CodeZen-2a673873401a80f9a674efea7c50a982",
+      githubLink: "https://github.com/FRONTENDBOOTCAMP-15th/vanilla-09-justCodeIt",
+      figmaLink: "",
+      deployUrl: "https://just-code-it-09.netlify.app/"
+    }
+  ]
+}
